1d470a09934af68addb0aac46f4354fc15140156
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-06-01 14:31:11 +0000

    Setup authentik fully with proper backups

---
+++ authentik/slc1-node-3/restore.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+HOMELAB_ROOT="${HOMELAB_ROOT:-$HOME/homelab}"
+
+# Source the function from manage_folder.sh
+. $HOMELAB_ROOT/backup/fn_restore_homelab.sh
+
+fn_restore_homelab "$HOMELAB_ROOT/authentik/slc1-node-3" "certs" "authentik-config"
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo "restore failed with status $result"
+else
+    echo "restore succeeded"
+fi
+
+fn_restore_homelab "$HOMELAB_ROOT/authentik/slc1-node-3" "custom-templates" "authentik-config"
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo "restore failed with status $result"
+else
+    echo "restore succeeded"
+fi
+
+fn_restore_homelab "$HOMELAB_ROOT/authentik/slc1-node-3" "media" "authentik-config"
+result=$?
+
+if [ $result -ne 0 ]; then
+    echo "restore failed with status $result"
+else
+    echo "restore succeeded"
+fi
