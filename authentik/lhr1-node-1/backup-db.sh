e1879154b542df0428fd8d78b4ed82021b970d54
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-06-03 22:40:22 +0000

    Beginning to setup backups

---
+++ authentik/lhr1-node-1/backup-db.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+HOMELAB_ROOT="${HOMELAB_ROOT:-$HOME/homelab}"
+
+docker compose exec -T postgres  pg_dumpall -c -U authentik-prod-1-admin | bzip2 --best > $HOMELAB_ROOT/authentik/lhr1-node-1/backups/dump_$(date +%Y-%m-%d_%H_%M_%S).sql.bz2
