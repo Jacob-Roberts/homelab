f21e85633ccf3f669170ec355b45356e5e71785a
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2024-06-04 20:42:19 +0000

    authentik backup

--- authentik/slc1-node-3/backup-db.sh
+++ authentik/slc1-node-3/backup-db.sh
@@ -4,4 +4,6 @@ set -e
 
 HOMELAB_ROOT="${HOMELAB_ROOT:-$HOME/homelab}"
 
-docker compose exec -T postgres  pg_dumpall -c -U authentik-prod-1-admin | bzip2 --best > $HOMELAB_ROOT/authentik/slc1-node-3/backups/dump_$(date +%Y-%m-%d_%H_%M_%S).sql.bz2
+docker compose -f $HOMELAB_ROOT/authentik/lhr1-node-1/compose.yaml exec -T postgres  pg_dumpall -c -U authentik-prod-1-admin | bzip2 --best > $HOMELAB_ROOT/authentik/slc1-node-3/backups/dump_$(date +%Y-%m-%d_%H_%M_%S).sql.bz2
+
+echo "successfully backed up authentik DB"
