e861b1af65630bfa0b0e06891a2e44725e6a2b2d
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-01-05 20:46:53 +0000

    Add omada controller to LHR1 and remove unifi controller

--- unifi/lhr1-node-1/init-mongo.js
+++
@@ -1,3 +0,0 @@
-// Before running, change the `MONGO_PASS` to the same value as in .env
-db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "MONGO_PASS", roles: [{role: "dbOwner", db: "unifi"}]});
-db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "MONGO_PASS", roles: [{role: "dbOwner", db: "unifi_stat"}]});
