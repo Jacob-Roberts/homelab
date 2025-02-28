6d7b5c4fc861063e7eb5912238d60d854ff3ead5
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-02-28 23:15:46 +0000

    Move syncthing to separate service

--- traefik/lhr1-node-1/extract-certs.sh
+++ traefik/lhr1-node-1/extract-certs.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-docker run -v ./config/certs:/certs -v ./certs-dump:/certs-dump ldez/traefik-certs-dumper file --clean --version v3 --source /certs/cloudflare-acme.json --dest /certs-dump
+mkdir -p certs-dump
+
+docker run --rm -v ./config/certs:/certs -v ./certs-dump:/certs-dump ldez/traefik-certs-dumper file --clean --domain-subdir=true --version v3 --source /certs/cloudflare-acme.json --dest /certs-dump
+
+mkdir -p synced-certs
+
+# Define the list of domains
+DOMAINS=(
+  "photos.jakerob.pro"
+  "authentik.lhr1.jakerob.pro"
+)
+
+for domain in "${DOMAINS[@]}"; do
+  sudo cp -f -r ./certs-dump/${domain} synced-certs/${domain}/
+done
+
+sudo chown -R jacobroberts:jacobroberts synced-certs/
+
+sudo rm -rf certs-dump
