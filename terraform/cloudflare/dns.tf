1ef3428639527993ef98d85689caf7f8fa0b27f5
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-04-30 21:54:10 +0100

    Some terraform changes

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -1,7 +1,3 @@
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
 ////////////////////////
 //  Vercel Records    //
 ////////////////////////
--- terraform/global/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_api_token" {
-  description = "Cloudflare API token"
-  type        = string
-}
-
-variable "cloudflare_zone_id" {
-  description = "value of zone_id from cloudflare"
-  type        = string
-}
