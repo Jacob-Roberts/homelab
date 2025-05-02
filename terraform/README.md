31e67301f2d11c65057989c5f74a3fa3d1159cbd
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-05-02 22:09:00 +0100

    Add tailscale terraform

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+You'll need to have a `terraform.tfvars` file. If you don't have that, then you probably don't have the correct state backup.
+
+To make a change, run `tofu plan`, and then `tofu apply`.
---
+++ terraform/tailscale/main.tf
@@ -0,0 +1,17 @@
+# Tailscale Provider
+
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    tailscale = {
+      source = "tailscale/tailscale"
+      version = "0.19.0"
+    }
+  }
+}
+
+provider "tailscale" {
+  oauth_client_id = var.tailscale_oauth_client_id
+  oauth_client_secret = var.tailscale_oauth_client_secret
+}
