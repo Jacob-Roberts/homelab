73ebc4d593bebf3bddeec2eb3d212341f1c7c23e
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-05-07 19:24:48 +0100

    Setup OCI for iad1

--- terraform/iad1/variables.tf
+++ terraform/iad1/variables.tf
@@ -1,9 +1,32 @@
-variable "compartment_id" {
-  description = "OCID from your tenancy page"
+variable "tenancy_ocid" {
+  description = "OCID of the tenancy"
   type        = string
+  sensitive = true
 }
+
+variable "user_ocid" {
+  description = "OCID of the user calling the API."
+  type        = string
+  sensitive = true
+}
+
+variable "rsa_private_key_path" {
+  description = "The path (including filename) of the private key stored on the computer."
+  type = string
+}
+
+variable "fingerprint" {
+  description = "Fingerprint for the key pair being used."
+  type = string
+}
+
 variable "region" {
   description = "region where you have OCI tenancy"
   type        = string
   default     = "us-ashburn-1"
+}
+
+variable "compartment_ocid" {
+  description = "Compartment ocid where to create all resources"
+  type        = string
 }
