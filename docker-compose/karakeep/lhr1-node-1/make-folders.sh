1e31d9030a5dd65dd744f662282bac1f7ead581d
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2025-12-18 21:00:55 +0000

    Karakeep

---
+++ docker-compose/karakeep/lhr1-node-1/make-folders.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# Get the directory of this script for reliable path resolution
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2003}" data meilisearch
