e177c18a22341a6688724fa826842c612d5f1790
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-01-04 04:53:35 +0000

    move updatable

--- terraform/cloudflare/jakerob.pro/dns.tf
+++ terraform/cloudflare/jakerob.pro/dns.tf
@@ -229,15 +229,6 @@ resource "cloudflare_dns_record" "lhr1-router" {
   content = "192.168.42.1"
 }
 
-resource "cloudflare_dns_record" "lhr1-hv-1" {
-  name    = "lhr1-hv-1.jakerob.pro"
-  zone_id = cloudflare_zone.jakerob_pro.id
-  proxied = false
-  ttl     = 1
-  type    = "A"
-  content = "192.168.42.10"
-}
-
 resource "cloudflare_dns_record" "lhr1-hv-2" {
   name    = "lhr1-hv-2.jakerob.pro"
   zone_id = cloudflare_zone.jakerob_pro.id
@@ -338,15 +329,6 @@ resource "cloudflare_dns_record" "slc1-node-3-vpn" {
 }
 
 /* Aliases */
-
-resource "cloudflare_dns_record" "proxmox-lhr1-cname" {
-  name    = "proxmox.lhr1.jakerob.pro"
-  zone_id = cloudflare_zone.jakerob_pro.id
-  proxied = false
-  ttl     = 1
-  type    = "CNAME"
-  content = "lhr1-hv-1.jakerob.pro"
-}
 
 resource "cloudflare_dns_record" "proxmox2-lhr1-cname" {
   name    = "proxmox2.lhr1.jakerob.pro"
