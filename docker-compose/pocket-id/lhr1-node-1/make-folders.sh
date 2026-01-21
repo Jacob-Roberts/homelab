0bd133f0a89672629f294addf1d74f2dd30b0f10
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-01-21 15:01:54 +0000

    Move pocket ID to a volume

--- docker-compose/pocket-id/lhr1-node-1/make-folders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Get the directory of this script for reliable path resolution
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2010}" data
