d85028fead1dfe25699d3d9e34713e268cd5fe1d
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-01-23 20:02:10 +0000

    Traefik iad1-node-1

--- docker-compose/traefik/iad1-node-1/make-folders.sh
+++ docker-compose/traefik/iad1-node-1/make-folders.sh
@@ -5,4 +5,4 @@ set -e
 # Get the directory of this script for reliable path resolution
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2001}" config-internal/certs/ config-internal/conf/ config-external/certs config-external/static-certs config-external/plugins-storage config-external/logs config-crowdsec crowdsec-db
+"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2001}" config-internal/certs/ config-internal/conf/ config-external/certs config-external/static-certs config-external/static-certs-slc1 config-external/plugins-storage config-external/logs config-crowdsec crowdsec-db
