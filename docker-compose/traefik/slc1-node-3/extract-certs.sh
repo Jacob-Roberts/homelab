2df6c8ddc1c95cecff47a132b4c19b7f7cb05e81
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-01-23 18:06:07 +0000

    Add extract certs script

---
+++ docker-compose/traefik/slc1-node-3/extract-certs.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This script runs inside the docker container,
+# so the paths won't look correct
+
+echo "Copying the tunneled certs to the synced-certs folder"
+
+# Define the list of domains
+DOMAINS="
+photos.autovation.com
+auth.autovation.com
+"
+
+for domain in $DOMAINS; do
+  echo "Copying $domain"
+  cp -r /output/$domain /synced-certs/
+done
+
+echo "Done copying certs, setting permissions"
+
+for domain in $DOMAINS; do
+  chgrp -R ${DOCKER_GROUP_ID_CERT_MANAGER:-2013} "/synced-certs/$domain"
+  chmod -R g+r /synced-certs/$domain
+done
+
+
