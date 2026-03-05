3762c7f6f2294ffe3c78dfa5b3acb01c15d96f5d
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-03-05 09:28:21 +0000

    Don't use fallback to .

--- docker-compose/calibre/lhr1-node-1/make-folders.sh
+++ docker-compose/calibre/lhr1-node-1/make-folders.sh
@@ -5,4 +5,4 @@ set -e
 # Get the directory of this script for reliable path resolution
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2006}" "${DOCKER_DATA_DIR:-.}/config" "${DOCKER_DATA_DIR:-.}/ingest" "${DOCKER_DATA_DIR:-.}/library"
+"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2006}" "${DOCKER_DATA_DIR}/calibre" "${DOCKER_DATA_DIR}/calibre/config" "${DOCKER_DATA_DIR}/calibre/ingest" "${DOCKER_DATA_DIR}/calibre/library"
