3f99487fc903cb4f65b0cc908faa79839acd847f
Jake Roberts <jroberts.g1ql8@simplelogin.com>
2026-07-05 23:17:30 +0000

    Move calibre

--- docker-compose/calibre/lhr1-node-1/make-folders.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Get the directory of this script for reliable path resolution
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-"$SCRIPT_DIR/../../make-folders.sh" "${DOCKER_USER_ID:-2006}:${DOCKER_USER_ID:-2006}" "${DOCKER_DATA_DIR}/calibre" "${DOCKER_DATA_DIR}/calibre/config" "${DOCKER_DATA_DIR}/calibre/ingest" "${DOCKER_DATA_DIR}/calibre/library"
